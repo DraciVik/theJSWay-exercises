3e81dfc52e58d93d2a5e961f63b9b0a08c4b2a70
DraciVik <viktor.stojanov90@gmail.com>
2018-02-24 22:56:55 +0100

    Excercises for Chapter 1 are complete

---
+++ Chapter-01/script.js
@@ -0,0 +1,28 @@
+/*Presentation
+Write a program that displays your name and age.*/
+
+let name = "Viktor";
+let age = 27;
+
+console.log(name);
+console.log(age);
+
+/*
+Minimalistic calculator
+Write a program that displays the results of adding, subtracting, multiplying and dividing 6 by 3.
+*/
+
+let a = 6;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+/*Values prediction
+Observe the following program and try to predict the values it displays.*/
+
+console.log(4 + 5); // Will display 9
+console.log("4 + 5"); //Will display 4 + 5
+console.log("4" + "5"); //Will display 45
