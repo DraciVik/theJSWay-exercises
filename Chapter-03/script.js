0d36c24b6625d7107ed398f7270ab9e8bcaceaec
DraciVik <viktor.stojanov90@gmail.com>
2018-02-25 23:52:11 +0100

    Finished the 'Number comparison' challenge

--- Chapter-03/script.js
+++ Chapter-03/script.js
@@ -27,3 +27,12 @@ switch (day.toLowerCase()) {
 		break;
     default:
 		console.log("That is not a valid day");
+  }
+
+  // Number comparison
+// Write a program that accepts two numbers, then compares their values and displays an appropriate message in all cases.
+
+let num1 = Number(prompt("Enter a number"));
+let num2 = Number(prompt("Enter another number"));
+
+num1 === num2 ? "They are equal" : num1 > num2 ? `${num1} is bigger than ${num2}` : `${num1} is smaller than ${num2}`
