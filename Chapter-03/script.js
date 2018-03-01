28d711f04f6445f2daf413122953e6fc2f67e3ff
DraciVik <viktor.stojanov90@gmail.com>
2018-03-01 15:52:59 +0100

    Finished with the Following Secons exercise. It took me a bit but all in all it was not that complicated

--- Chapter-03/script.js
+++ Chapter-03/script.js
@@ -81,4 +81,21 @@ const monthsInYear = {
 };
 const monthNumber = Number(prompt("Enter a month number"));
 
-monthNumber <= 12 && monthNumber >=0 ? console.log(`The month you entered (${monthNumber}) has ${monthsInYear[monthNumber]} days`) : console.log("please enter a valid number");
+monthNumber <= 12 && monthNumber >= 0 ? console.log(`The month you entered (${monthNumber}) has ${monthsInYear[monthNumber]} days`) : console.log("please enter a valid number");
+
+
+// Following second
+// Write a program that asks for a time under the form of three information (hours, minutes, seconds). The program calculates and shows the time one second after. Incorrect inputs must be taken into account.
+let hours = Number(prompt("Enter the hour"));
+let minutes = Number(prompt("Enter the minute"));
+let seconds = Number(prompt("Enter the seconds"));
+
+if ((seconds === 59) && (minutes === 59) && (hours === 23)) {
+      console.log(`00h00m00s`);
+    } else if ((minutes === 59) && (seconds === 59)) {
+      console.log(`${hours += 1}h${minutes = 00}m${seconds = 00}s`);
+    } else if (seconds === 59) {
+      console.log(`${hours}h${minutes += 1}m${seconds = 00}s`);
+    } else {
+      console.log(`${hours}h${minutes}m${seconds += 1}s`);
+}
