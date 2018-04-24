29ad469dcae87fc1c0c1111641632ca7f2e6f2fe
DraciVik <viktor.stojanov90@gmail.com>
2018-04-24 22:43:46 +0200

    Finished the Carousel coding challenge

---
+++ Chapter-04/script.js
@@ -0,0 +1,17 @@
+/*
+Carousel
+Write a program that launches a carousel for 10 turns, showing the turn number each time.
+*/
+
+for(let i = 1; i <= 10; i++) {
+  console.log(`turn number ${i}`);
+};
+//When it's done, improve it so that the number of turns is given by the user.
+let userPrompt = prompt("Enter the number of turns");
+for(let i = 1; i <= userPrompt; i++) {
+  if(typeof Number(userPrompt) === "number"){
+  console.log(`Turn number ${i}`);
+} else {
+  console.log("Enter a valid number");
+}
+};
