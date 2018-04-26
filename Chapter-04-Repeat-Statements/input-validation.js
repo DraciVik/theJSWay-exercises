dc9a7b8048d8b45122d41d1128e53427fcc813af
DraciVik <viktor.stojanov90@gmail.com>
2018-04-26 22:36:12 +0200

    Finished a section of the challenge. Need to make it work for more complexity

--- Chapter-04-Repeat-Statements/input-validation.js
+++ Chapter-04-Repeat-Statements/input-validation.js
@@ -7,6 +7,6 @@ for(let i = Number(prompt("Enter a number that is less than or equal to 100."));
 
 /*When you are done with the above, improve the program so that the terminating number is between 50 and 100.*/
 
-for(let i = Number(prompt("Enter a number that is less than or equal to 100.")); i > 100 || i < 50; i) {
-  i = Number(prompt("Enter a number that is less than or equal to 100."));
+for(let i = Number(prompt("Enter a number that is less than or equal to 100 and equal or higher than 50")); i > 100 || i < 50; i) {
+  i = Number(prompt("Enter a number that is less than or equal to 100 and equal or higher than 50"));
 };
