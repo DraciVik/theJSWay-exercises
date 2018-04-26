e87a76ffb43bdf59a224f7b6eb41f2f00ea1fa5c
DraciVik <viktor.stojanov90@gmail.com>
2018-04-26 22:48:56 +0200

    Initialized a readme.md

---
+++ Chapter-04-Repeat-Statements/multiplication-table.js
@@ -0,0 +1,12 @@
+//Multiplication table
+//Write a program that asks the user for a number, then shows the multiplication table for this number.
+let n = 0
+for(let i = Number(prompt("Enter a number please")); n <= 10; n++) {
+  console.log(i * n);
+};
+
+//When you are done, improve the program so it only accepts numbers between 2 and 9 (use the previous exercise as a blueprint).
+for(let i = Number(prompt("Enter a number please")); i < 2 || i > 9; i) {
+  i = Number(prompt("Enter a number please"));
+
+};
