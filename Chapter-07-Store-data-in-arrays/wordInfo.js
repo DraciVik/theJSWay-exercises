7f5ec6cc1664cdb737ec626078aa327ea263a1c8
DraciVik <viktor.stojanov90@gmail.com>
2018-05-16 23:21:09 +0200

    Finished the 'Word info' challenge

---
+++ Chapter-07-Store-data-in-arrays/wordInfo.js
@@ -0,0 +1,8 @@
+/*
+Word info
+Write a program that asks you for a word then shows its length, lowercase, and uppercase values.
+*/
+const word = prompt("Enter a random word please!");
+const lowerCaseWord = word.toLowerCase();
+const upperCaseWord = word.toUpperCase();
+console.log(lowerCaseWord, upperCaseWord);
