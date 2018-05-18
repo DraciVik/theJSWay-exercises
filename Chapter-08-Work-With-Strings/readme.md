a1cdd852d15ad022f4df75134b44485169c8ae64
DraciVik <viktor.stojanov90@gmail.com>
2018-05-18 23:57:09 +0200

    Added links to the readme.md file for the completed challenges of the 8th chapter

---
+++ Chapter-08-Work-With-Strings/readme.md
@@ -0,0 +1,4 @@
+* [Vowel count](https://github.com/DraciVik/theJSWay-exercises/blob/master/Chapter-08-Work-With-Strings/vowelCount.js)
+* [Word info](https://github.com/DraciVik/theJSWay-exercises/blob/master/Chapter-08-Work-With-Strings/wordInfo.js)
+* [Backwards word](https://github.com/DraciVik/theJSWay-exercises/blob/master/Chapter-08-Work-With-Strings/backwardsWord.js)
+* [Palindrome](https://github.com/DraciVik/theJSWay-exercises/blob/master/Chapter-08-Work-With-Strings/palindrome.js)
---
+++ Chapter-09-Understand-Object-oriented-Programming/account_list.js
@@ -0,0 +1,23 @@
+// /*
+// Account list
+// Let's build upon a previous account object exercise. A bank account is still defined by:
+//
+// A name property.
+// A balance property, initially set to 0.
+// A credit method adding the value passed as an argument to the account balance.
+// A describe method returning the account description.
+// Write a program that creates three accounts: one belonging to Sean, another to Brad and the third one to Georges. These accounts are stored in an array. Next, the program credits 1000 to each account and shows its description.
+// */
+//
+// class Account {
+//   constructor(name) {
+//     this.name = name;
+//     this.balance = 0;
+//   }
+//   credit() {
+//     this.balance += balance;
+//   }
+//   describe() {
+//     return `owner`
+//   }
+// }
