0dab4bd8712633e2a36892e12f0bea9a92dec82c
DraciVik <viktor.stojanov90@gmail.com>
2018-05-20 23:59:47 +0200

    Finished the Government forms challenge. Need to refactor

---
+++ Chapter-10-Discover-Functional-Programming/government_forms.js
@@ -0,0 +1,39 @@
+/*
+Government forms
+Complete the following program to compute and show the names of political forms ending with "cy".
+
+const governmentForms = [
+  {
+    name: "Plutocracy",
+    definition: "Rule by the wealthy"
+  },
+  {
+    name: "Oligarchy",
+    definition: "Rule by a small number of people"
+  },
+  {
+    name: "Kleptocracy",
+    definition: "Rule by the thieves"
+  },
+  {
+    name: "Theocracy",
+    definition: "Rule by a religious elite"
+  },
+  {
+    name: "Democracy",
+    definition: "Rule by the people"
+  },
+  {
+    name: "Autocracy",
+    definition: "Rule by a single person"
+  }
+];
+
+// TODO: compute the formsEndingWithCy array
+
+// Should show ["Plutocracy", "Kleptocracy", "Theocracy", "Democracy", "Autocracy"]
+console.log(formsEndingWithCy);
+*/
+const formsEndingWithCy = forms => {
+  return forms.filter(names => names.endsWith("cy"));
+}
