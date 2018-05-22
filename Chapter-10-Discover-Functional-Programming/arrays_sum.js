48e3f218412b8f69aa57afcee04f0d9806fd447d
DraciVik <viktor.stojanov90@gmail.com>
2018-05-22 23:28:45 +0200

    Finished the Arrays Sum challenge

---
+++ Chapter-10-Discover-Functional-Programming/arrays_sum.js
@@ -0,0 +1,14 @@
+/*
+Arrays sum
+Complete the following program to compute and show the total sum of the values in each of the arrays.
+
+const arrays = [[1, 4], [11], [3, 5, 7]];
+
+// TODO: compute the value of the arraysSum variable
+
+console.log(arraysSum); // Should show 31
+*/
+
+const arrays = [[1, 4], [11], [3, 5, 7]];
+const flattenedArray = arrays.reduce((lvl1, lvl2) =>  lvl1.concat(lvl2), []);
+const arraySum = flattenedArray.reduce((acc, value) => acc + value, 0);
