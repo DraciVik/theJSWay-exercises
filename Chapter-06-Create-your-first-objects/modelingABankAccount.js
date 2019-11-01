42954c90ca94c7b3644c529e45e1955045e5fc30
DraciVik <viktor.stojanov90@gmail.com>
2019-11-01 22:59:24 +0100

    Minor typo

--- Chapter-06-Create-your-first-objects/modelingABankAccount.js
+++ Chapter-06-Create-your-first-objects/modelingABankAccount.js
@@ -14,9 +14,9 @@ const account = {
   name: "Alex",
   balance: 0,
   credit(value) {
-    this.balance += value;;
+    this.balance += value;
   },
   describe() {
-    return `owner: ${this.name}, balance: ${this.balance}`
+    return `owner: ${this.name}, balance: ${this.balance}`;
   }
 };
